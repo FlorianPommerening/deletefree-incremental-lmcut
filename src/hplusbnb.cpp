106b02ba3f078f30a4203d60f36ac94eb1314a7c
Flogo <Flogo@Eumeralla>
2011-06-01 14:02:13 +0200

    bugfix priority Q: hmax was not changed in push moved cpp-module to more python friendly cppmodule cpp module adapter in python changed format a bit

---
+++ src/hplusbnb.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <ostream>
+#include <sstream>
+
+#include "Variable.h"
+#include "RelaxedOperator.h"
+#include "RelaxedTask.h"
+#include "hmax.h"
+
+using namespace std;
+
+void intToString(int i, string& res) {
+    ostringstream temp;
+    temp << i;
+    temp.str(res);
+}
+
+int main() {
+    RelaxedTask parsedTask;
+    try {
+        parsedTask.parseFile("test.task");
+    } catch (const char *e) {
+        cout << e << endl;
+        return 1;
+    }
+
+    parsedTask.crossreference();
+    cout << hmax(parsedTask) << endl;
+
+    cout << "done" << endl;
+    return 0;
+}
