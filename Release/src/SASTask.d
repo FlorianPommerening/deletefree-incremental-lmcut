75a481308aefce1d3fc30309f1fbe45f3399cf3f
Flogo <Flogo@Eumeralla>
2011-06-07 22:03:16 +0200

    prepared for grid

---
+++ Release/src/SASTask.d
@@ -0,0 +1,13 @@
+src/SASTask.d src/SASTask.o: ../src/SASTask.cpp ../src/SASTask.h \
+ ../src/RelaxedTask.h ../src/Variable.h ../src/RelaxedOperator.h \
+ ../src/foreach.h
+
+../src/SASTask.h:
+
+../src/RelaxedTask.h:
+
+../src/Variable.h:
+
+../src/RelaxedOperator.h:
+
+../src/foreach.h:
---
+++ Release/src/hplusbnb.d
@@ -0,0 +1,17 @@
+src/hplusbnb.d src/hplusbnb.o: ../src/hplusbnb.cpp ../src/Variable.h \
+ ../src/RelaxedOperator.h ../src/RelaxedTask.h ../src/hmax.h \
+ ../src/SASTask.h ../src/foreach.h ../src/Timer.h
+
+../src/Variable.h:
+
+../src/RelaxedOperator.h:
+
+../src/RelaxedTask.h:
+
+../src/hmax.h:
+
+../src/SASTask.h:
+
+../src/foreach.h:
+
+../src/Timer.h:
