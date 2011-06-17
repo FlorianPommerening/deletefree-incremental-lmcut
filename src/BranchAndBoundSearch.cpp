25baba1eeac083df90469567a55c9c8a0ef2c4eb
Flogo <Flogo@Eumeralla>
2011-06-17 19:35:44 +0200

    bugfix: forgot braces around if statement + Full debug output

--- src/BranchAndBoundSearch.cpp
+++ src/BranchAndBoundSearch.cpp
@@ -4,6 +4,11 @@
 #include <iostream>
 using namespace std;
 
+// very heavy on the output, use only for bug tracing
+// #define FULL_DEBUG
+
+void printNode(SearchNode &searchNode, UIntEx &upperBound);
+
 BranchAndBoundSearch::BranchAndBoundSearch(RelaxedTask &task, OperatorSelector &operatorSelector) :
     task(task),
     operatorSelector(operatorSelector),
@@ -17,15 +22,23 @@ UIntEx BranchAndBoundSearch::run() {
 }
 
 UIntEx BranchAndBoundSearch::recursiveBranchAndBound(SearchNode &searchNode) {
-    // very heavy on the output, use only for bug tracing
-    // printNode(searchNode, this->costUpperBound);
-
+#ifdef FULL_DEBUG
+    printNode(searchNode, this->costUpperBound);
+#endif
     if (searchNode.getCostLowerBound() >= this->costUpperBound) {
+#ifdef FULL_DEBUG
+        cout << endl << "Exceeded bound (" << searchNode.getCostLowerBound() << " >= " << this->costUpperBound << ")" << endl << endl;
+#endif
         return INFINITY;
     }
     // TODO could optimize this by saving goal in every VariableSet and setting a flag as soon as goal is added
     if (searchNode.currentState.contains(this->task.goal)) {
         this->costUpperBound = searchNode.currentCost;
+        this->plan.clear();
+        this->plan.assign(searchNode.partialPlan.begin(), searchNode.partialPlan.end());
+#ifdef FULL_DEBUG
+        cout << endl << "New Solution, updated bound (" << this->costUpperBound << ")" << endl << endl;
+#endif
         return this->costUpperBound;
     }
 
@@ -33,6 +46,9 @@ UIntEx BranchAndBoundSearch::recursiveBranchAndBound(SearchNode &searchNode) {
     bool addFirst = true;
     this->operatorSelector.select(searchNode, this->costUpperBound, &nextOperator, &addFirst);
     if (nextOperator == NULL) {
+#ifdef FULL_DEBUG
+        cout << endl << "No more applicable operators. Backtracking" << endl << endl;
+#endif
         return INFINITY;
     }
     // TODO remove redundant operators???
@@ -43,25 +59,41 @@ UIntEx BranchAndBoundSearch::recursiveBranchAndBound(SearchNode &searchNode) {
         if (iSuc > 1) throw "There are only two successors. Do not call the loop more often!";
         bool first = (iSuc == 0);
         if (first ^ addFirst) {
+#ifdef FULL_DEBUG
+            cout << endl << "Forbidding operator " << nextOperator->name << endl;
+#endif
             successor = new SearchNode(searchNode);
             successor->ForbidOperator(nextOperator);
         } else {
+#ifdef FULL_DEBUG
+            cout << endl << "Applying operator " << nextOperator->name << endl;
+#endif
             successor = new SearchNode(searchNode);
             successor->ApplyOperator(nextOperator);
         }
         UIntEx planCost = this->recursiveBranchAndBound(*successor);
         if (planCost != INFINITY) {
             foundBetterPlan = true;
-            if (searchNode.getCostLowerBound() >= this->costUpperBound)
+            if (searchNode.getCostLowerBound() >= this->costUpperBound) {
+#ifdef FULL_DEBUG
+                cout << endl << "Found perfect solution for this subtree, ignoring second successor" << endl;
+#endif
                 delete successor; successor = NULL;
                 break;
+            }
         }
         delete successor; successor = NULL;
     }
 
     if (foundBetterPlan)
+#ifdef FULL_DEBUG
+        cout << endl << "Backtracking with better plan of cost " << this->costUpperBound << endl;
+#endif
         return this->costUpperBound;
     // else
+#ifdef FULL_DEBUG
+    cout << endl << "Backtracking without better plan" << endl;
+#endif
     return INFINITY;
 }
 
